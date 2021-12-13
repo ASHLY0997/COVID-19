ceb4e69047718611a6148adcfc5b3a52032a5e02
ASHLY0997 <83544852+ASHLY0997@users.noreply.github.com>
2021-12-13 14:46:33 +0530

    Add files via upload

---
+++ services.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+
+  include 'header.php';
+?>
+<h1 style="margin-top: 70px;margin-left: 125px;">Our Services</h1>
+<div style="display: inline;">
+
+  <a href="login.php">
+<div style="width: 350px;height: 250px;background-color: #20d8da;
+;margin: 125px;display: inline-block;border-radius: 10px 10px 10px 10px;border-color: #f9d700;border: solid;">
+
+  <div style="margin: 50px;">
+
+    <h1 style="color: black">LOGIN</h1><br>
+
+
+
+    
+
+  </div>
+  
+</div>
+</a>
+
+<a href="register.php">
+<div style="width: 350px;height: 250px;background-color: #20d8da;
+;margin: 125px;display: inline-block;border-radius: 10px 10px 10px 10px;border-color: #f9d700;border: solid;">
+
+  <div style="margin: 50px;">
+
+    <h1 style="color: black">REGISTER</h1><br>
+
+
+
+    
+
+  </div>
+  
+</div>
+</a>
+
+<a href="login.php">
+<div style="width: 350px;height: 250px;background-color: #20d8da;
+;margin: 125px;position: absolute;display: inline-block;border-radius: 10px 10px 10px 10px;border-color: #000;margin-top: 110px;border: solid;">
+
+  <div style="margin: 50px;">
+
+    <h1 style="color: black">HEALTH FACILITIES</h1><br>
+
+
+
+    
+
+  </div>
+  
+</div>
+</a>
+</div>
+
+ <br><br><br><br><br><br><br>
+   <?php
+       
+
+         include 'footer.php'
+    ?>
+
+</html>
