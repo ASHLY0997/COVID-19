085545fe0d22576cfdcbc88c363f5fbf900dc7a0
ASHLY0997 <83544852+ASHLY0997@users.noreply.github.com>
2021-12-13 14:48:03 +0530

    Add files via upload

---
+++ insta.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+
+  include 'header.php';
+?>
+
+<section style="height: 430px;margin: 200px;">
+  
+
+  <table class="table" style="margin-left: 100px;margin-top: 50px;border: none;width: 600px;background-color: #7c7979;border-radius: 10px 10px 10px 10px;color: white;">
+
+
+
+
+
+
+<h1 style="margin-top: 20px;margin-left: 25px;">Instructions</h1>
+<div style="display: inline;">
+
+  <a href="login.php">
+<div style="margin: 125px;display: inline-block;border-radius: 10px 10px 10px 10px;">
+
+  <div style="margin: 50px;">
+
+    <h2><p style="color: black;">INSTRUCTIONS<br>
+1 Register to book your bed through the website BreakTheChain.com<br>
+2 Submit the mandatory details of contact in the registration page<br>
+3 Citizen must carry the same original ID proof submitted during registration for the admission procedures<br>
+4 Enter the name as per the ID proof<br>
+5 Enter the year of birth as per the ID proof<br>
+6 Select the gender (Male/Female/Others)</p></h2>
+
+
+
+    
+
+  </div>
+  
+</div>
+</a>
+
+
+
+
+</div>
+
+ <br><br><br><br><br><br><br>
+   <?php
+       
+
+         include 'footer.php'
+    ?>
+
+</html>
+
