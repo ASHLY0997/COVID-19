fd80bf492ccde2e6ffd94fec4709d68bf46cae62
ASHLY0997 <83544852+ASHLY0997@users.noreply.github.com>
2021-12-13 14:45:17 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+
+  include 'header.php';
+?>
+
+<section style="height: 430px;margin: 200px;">
+  <img src="img\about-img1.png" width="400px" style="float: left;margin-right: 200px; ">
+<h1 style="margin-left: 100px;">About Us</h1>
+  <table class="table" style="margin-left: 100px;margin-top: 50px;border: none;width: 600px;background-color: #7c7979;border-radius: 10px 10px 10px 10px;color: white;">
+
+
+
+
+            
+           <p>eHealth is the pioneer project funded by Government of India and Department of Health and Family Welfare, Government of Kerala, designed to provide citizens of Kerala with a convenient centralized healthcare system.</p>
+
+
+
+
+
+</tr>
+                     </tr>
+                     
+                    
+                     
+               
+    
+</table>
+<br><br>
+
+        
+      </section>
+
+
+ <?php
+       
+
+         include 'footer.php';
+    ?>
+
+</html>
